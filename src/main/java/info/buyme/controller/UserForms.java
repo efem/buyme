55da1ebb20477e3495ded9d9bd56c3b4c45b350c
efem-pc <qlkid@o2.pl>
2016-05-29 21:45:32 +0200

    JSON Ok

--- src/main/java/info/buyme/controller/UserForms.java
+++ src/main/java/info/buyme/controller/UserForms.java
@@ -1,9 +1,14 @@
 package info.buyme.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +19,16 @@ public class UserForms {
 	@RequestMapping("/new")
 	public ModelAndView showNewUserForm() {
 		return new ModelAndView("newUserForm");
+	}
+	
+	@RequestMapping(value = "/jason", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody List<String> showJason() {
+		logger.debug("INSIDE /JASON");
+		final List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("Start");
+        return list;
+		//return new String("jabba");
 	}
 	
 
